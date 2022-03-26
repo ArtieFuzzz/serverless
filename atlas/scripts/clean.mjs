389710dbe3f37b68e695e2f6bc429860ddebe647
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-03-26 21:38:29 +1100

    Update atlas

--- atlas/scripts/clean.mjs
+++ atlas/scripts/clean.mjs
@@ -1,16 +1,8 @@
 import fse from 'fs-extra'
 import { join } from 'path'
 
-const files = ['index.js', 'index.js.map']
-
-for (const file in files){
-  if (!fse.existsSync(join(process.cwd(), file))) {
-    process.exit(0)
-  }
+if (!fse.existsSync(join(process.cwd(), 'build'))) {
+  process.exit(0)
 }
 
-await fse.rm(join(process.cwd(), 'tsconfig.tsbuildinfo'))
-
-for (const file in files){
-  await fse.rm(join(process.cwd(), file))
-}
+await fse.rm(join(process.cwd(), 'build'), { recursive: true })
